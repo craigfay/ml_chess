421a4755e7b09de85deea54f9013dd1b08fe0967
craig fay <craigfay1@gmail.com>
2020-03-17 21:25:38 -0500

    Configure for cbindgen

--- rust_code/src/lib.rs
+++ rust_code/src/lib.rs
@@ -1,10 +1,5 @@
 use chess_engine::GameState;
 
-fn main() {
-    let state = GameState::new();
-    println!("{}", state.to_string());
-}
-
 #[no_mangle]
 pub extern "C" fn sum(a: i32, b: i32) -> i32 {
     a + b
