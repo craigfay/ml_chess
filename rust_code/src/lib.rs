8901be4b761764b9b99a4a4b75d475efd1778677
craig fay <craigfay1@gmail.com>
2020-03-18 22:03:44 -0500

    Rename function names

--- rust_code/src/lib.rs
+++ rust_code/src/lib.rs
@@ -15,26 +15,26 @@ use chess_engine::{
     },
 };
 
-pub extern "C" fn bitboard() -> Vec<i32> {
+pub extern "C" fn gamestate_as_ints() -> Vec<i32> {
     let mut result = vec![];
 
     let state = GameState::new();
     for index in 0..64 {
         let maybe_piece = state.squares[index];
-        result.push(convert_piece_to_int(maybe_piece));
+        result.push(piece_as_int(maybe_piece));
     }
     result
 }
 
 #[test]
-fn bitboard_test() {
-    let b = bitboard();
+fn gamestate_as_ints_test() {
+    let ints = gamestate_as_ints();
     let expected = vec![4, 3, 2, 5, 6, 2, 3, 4, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7, 7, 7, 10, 9, 8, 11, 12, 8, 9, 10];
 
-    assert_eq!(b, expected);
+    assert_eq!(ints, expected);
 }
 
-fn convert_piece_to_int(maybe_piece: Option<Piece>) -> i32 {
+fn piece_as_int(maybe_piece: Option<Piece>) -> i32 {
     match maybe_piece {
         None => 0,
         Some(piece) => {
@@ -57,44 +57,44 @@ fn convert_piece_to_int(maybe_piece: Option<Piece>) -> i32 {
 }
 
 #[test]
-fn convert_piece_to_int_test() {
-    assert_eq!(0, convert_piece_to_int(None));
+fn piece_as_int_test() {
+    assert_eq!(0, piece_as_int(None));
 
     let piece = Piece { color: White, name: Pawn };
-    assert_eq!(1, convert_piece_to_int(Some(piece)));
+    assert_eq!(1, piece_as_int(Some(piece)));
 
     let piece = Piece { color: White, name: Bishop };
-    assert_eq!(2, convert_piece_to_int(Some(piece)));
+    assert_eq!(2, piece_as_int(Some(piece)));
 
     let piece = Piece { color: White, name: Knight };
-    assert_eq!(3, convert_piece_to_int(Some(piece)));
+    assert_eq!(3, piece_as_int(Some(piece)));
 
     let piece = Piece { color: White, name: Rook };
-    assert_eq!(4, convert_piece_to_int(Some(piece)));
+    assert_eq!(4, piece_as_int(Some(piece)));
 
     let piece = Piece { color: White, name: Queen };
-    assert_eq!(5, convert_piece_to_int(Some(piece)));
+    assert_eq!(5, piece_as_int(Some(piece)));
 
     let piece = Piece { color: White, name: King };
-    assert_eq!(6, convert_piece_to_int(Some(piece)));
+    assert_eq!(6, piece_as_int(Some(piece)));
 
     let piece = Piece { color: Black, name: Pawn };
-    assert_eq!(7, convert_piece_to_int(Some(piece)));
+    assert_eq!(7, piece_as_int(Some(piece)));
 
     let piece = Piece { color: Black, name: Bishop };
-    assert_eq!(8, convert_piece_to_int(Some(piece)));
+    assert_eq!(8, piece_as_int(Some(piece)));
 
     let piece = Piece { color: Black, name: Knight };
-    assert_eq!(9, convert_piece_to_int(Some(piece)));
+    assert_eq!(9, piece_as_int(Some(piece)));
 
     let piece = Piece { color: Black, name: Rook };
-    assert_eq!(10, convert_piece_to_int(Some(piece)));
+    assert_eq!(10, piece_as_int(Some(piece)));
 
     let piece = Piece { color: Black, name: Queen };
-    assert_eq!(11, convert_piece_to_int(Some(piece)));
+    assert_eq!(11, piece_as_int(Some(piece)));
 
     let piece = Piece { color: Black, name: King };
-    assert_eq!(12, convert_piece_to_int(Some(piece)));
+    assert_eq!(12, piece_as_int(Some(piece)));
 }
 
 
