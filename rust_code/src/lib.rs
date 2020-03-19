622c6bb48951c4ee8d4514f6ac6deedd494e7673
craig fay <craigfay1@gmail.com>
2020-03-18 21:53:04 -0500

    test convert_piece_to_int()

--- rust_code/src/lib.rs
+++ rust_code/src/lib.rs
@@ -52,8 +52,45 @@ fn convert_piece_to_int(maybe_piece: Option<Piece>) -> i32 {
 
 #[test]
 fn convert_piece_to_int_test() {
+    assert_eq!(0, convert_piece_to_int(None));
+
     let piece = Piece { color: White, name: Pawn };
     assert_eq!(1, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: White, name: Bishop };
+    assert_eq!(2, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: White, name: Knight };
+    assert_eq!(3, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: White, name: Rook };
+    assert_eq!(4, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: White, name: Queen };
+    assert_eq!(5, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: White, name: King };
+    assert_eq!(6, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: Black, name: Pawn };
+    assert_eq!(7, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: Black, name: Bishop };
+    assert_eq!(8, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: Black, name: Knight };
+    assert_eq!(9, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: Black, name: Rook };
+    assert_eq!(10, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: Black, name: Queen };
+    assert_eq!(11, convert_piece_to_int(Some(piece)));
+
+    let piece = Piece { color: Black, name: King };
+    assert_eq!(12, convert_piece_to_int(Some(piece)));
+
+
 }
 
 
