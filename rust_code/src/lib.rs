53f804a5300e3e725f18f32af0890a14e6a91118
craig fay <craigfay1@gmail.com>
2020-03-18 21:36:24 -0500

    WIP add bitboard function

--- rust_code/src/lib.rs
+++ rust_code/src/lib.rs
@@ -1,5 +1,17 @@
 use chess_engine::GameState;
 
+#[test]
+fn bitboard_test() {
+    let b = bitboard();
+    assert_eq!(b, vec![1,2,3]);
+}
+
+pub extern "C" fn bitboard() -> Vec<i32> {
+    let state = GameState::new();
+    vec![1,2,3]
+}
+
+
 #[no_mangle]
 pub extern "C" fn sum(a: i32, b: i32) -> i32 {
     a + b
