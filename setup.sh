eddf84a5f14f7937efcd0946fed0ffd6ac4fa1b0
craig fay <craigfay1@gmail.com>
2020-03-19 21:21:32 -0500

    Add compilation steps to setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+# Compile Rust Crate to shared library
+cd /app/rust_code
+cargo build
+
+# Copy shared library
+cp /app/rust_code/target/debug/libml_chess.so /usr/local/lib/libml_chess.so
+
+# Generate header files
+cbindgen --output /usr/local/include/ml_chess/c_api.h
+
